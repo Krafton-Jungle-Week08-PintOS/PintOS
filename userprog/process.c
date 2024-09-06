7b17d2a2867528a8e94881021f917561e3c6ba54
yongsoung <dydrkfl223@gmail.com>
2024-09-06 21:19:45 +0900

    test

--- userprog/process.c
+++ userprog/process.c
@@ -204,6 +204,7 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+	// 주석
 	return -1;
 }
 
